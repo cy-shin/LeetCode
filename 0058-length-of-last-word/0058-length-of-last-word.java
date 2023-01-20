478bbe262d77138035fb199c332802b79bbb466b
cy-shin <110644369+cy-shin@users.noreply.github.com>
2023-01-20 11:56:08 +0900

    Time: 0 ms (100.00%), Space: 40.5 MB (71.15%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,8 +1,21 @@
 class Solution {
     public int lengthOfLastWord(String s) {
 
-        String[] a = s.split(" ");
-        return a[a.length-1].length(); 
+        int count = 0;
+        int end = s.length();
+
+        while(s.charAt(end-1)==' '){
+            end--;
+            if(end==0) return 0;
+        }
+
+        while(s.charAt(end-1)!=' '){
+            count++;
+            end--;
+            if(end==0) break;
+        }
+
+        return count; 
 
     }
 }
