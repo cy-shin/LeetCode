b36c8b602393e6b055953cd6e9a7aedbc8126eb2
cy-shin <110644369+cy-shin@users.noreply.github.com>
2023-02-03 10:04:48 +0900

    Time: 695 ms (74.90%), Space: 0B (100.00%) - LeetHub

--- 0175-combine-two-tables/0175-combine-two-tables.sql
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -1,3 +1,5 @@
-SELECT firstName, lastName, city, state
-FROM "PERSON" P
-LEFT JOIN "ADDRESS" A USING(personId);
+/* Write your PL/SQL query statement below */
+SELECT firstName, lastName,
+       (SELECT city from Address A WHERE P.personId = A.personId) as city,
+       (SELECT state from Address A WHERE P.personId = A.personId) as state
+FROM Person P;
