c781b21fc4f122d1f5f6825a54b66b6f81a8c5b5
cy-shin <110644369+cy-shin@users.noreply.github.com>
2023-02-16 00:03:40 +0900

    Time: 1 ms (100.00%), Space: 42.5 MB (66.13%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -4,11 +4,14 @@ class Solution {
 
         int[] count = new int[26];
         
-        for(int i=0; i<s.length(); i++){
-            count[s.charAt(i)-'a']++;
-            count[t.charAt(i)-'a']--;            
+        for(char ch : s.toCharArray()){
+            count[ch - 'a']++;
         }
-        
+
+        for(char ch : t.toCharArray()){
+            count[ch - 'a']--;
+        }
+
         for(int i=0; i<count.length; i++){
             if(count[i] != 0) return false;
         }
