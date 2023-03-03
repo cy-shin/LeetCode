2a42c045acf63a4e9c24e3b46b3b7a7efc1e503e
cy-shin <110644369+cy-shin@users.noreply.github.com>
2023-03-04 00:13:10 +0900

    Time: 0 ms (100.00%), Space: 45.9 MB (29.20%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,27 +1,51 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
+        int start = searchStart(nums, target);
+        int end = searchEnd(nums, target);
+        int[] result = {start, end};
+        return result;    
+    }
+    public int searchStart(int[] nums, int target){
+        int index = -1;
+        
         int left = 0;
         int right = nums.length-1;
-        int start = -1;
-        int end = -1;
         
-        while(left<=right){
-            if(nums[left]==target && nums[right]==target){
-                start = left;
-                end = right;
-                break;
-            } else if(nums[left]!=target){
-                left++;
-            } else if(nums[right]!=target){
-                right--;
+        while(left <= right){
+            int mid = left + (right - left)/2;
+
+            if(nums[mid]>=target){
+                right = mid - 1;
             } else {
-                left++;
-                right--;
+                left = mid + 1;
+            }
+            if(nums[mid]==target){
+                index = mid;
             }
         }
 
-        int[] result = {start, end};
+        return index;
+    }
 
-        return result;
+    public int searchEnd(int[] nums, int target){
+        int index = -1;
+        
+        int left = 0;
+        int right = nums.length-1;
+        
+        while(left <= right){
+            int mid = left + (right - left)/2;
+
+            if(nums[mid]<=target){
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+            if(nums[mid]==target){
+                index = mid;
+            }
+        }
+
+        return index;
     }
 }
