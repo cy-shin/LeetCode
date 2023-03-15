51060058bd1515acd5cfc28a7a3acb13e58df2fc
cy-shin <110644369+cy-shin@users.noreply.github.com>
2023-03-15 09:16:37 +0900

    Time: 2 ms (34.75%), Space: 42.8 MB (28.78%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -9,7 +9,7 @@ class Solution {
 
     public void permute(int[] nums, List<Integer> list, List<List<Integer>> resultList){
         if(list.size () == nums.length){
-            resultList.add(new ArrayList<>(new ArrayList<>(list)));
+            resultList.add(new ArrayList<>(list));
         } else {
             for(int i=0; i<nums.length; i++){
                 if(list.contains(nums[i])) continue;
